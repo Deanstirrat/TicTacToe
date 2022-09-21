7b8fe83a460026993b394e5dcbc0625b96b41510
Dean Stirrat <deanstirrat@gmail.com>
2022-09-21 12:36:52 -0700

    finished single player game. added framework for implementing ai game later

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const playBtn = document.getElementById('playBtn');
+const singlePlayerButton = document.getElementById('singlePlayerButton')
+const twoPlayerButton = document.getElementById('twoPlayerButton')
+const continueBtn = document.getElementById('continueBtn')
 const gameSelect = document.getElementById('gameSelectModal')
 const overlay = document.getElementById('overlay')
+const turnDisplay = document.getElementById('turnDisplay')
+const winnerModal = document.getElementById('winnerModal')
+const winnerHeading = document.getElementById('winnerHeading')
+
 
 const box1 = document.getElementById('1')
 const box2 = document.getElementById('2')
@@ -15,7 +22,63 @@ const box9 = document.getElementById('9')
 const board = (() => {
     let realBoxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
     let playerTurn = 'X'
+    turnDisplay.textContent = 'X\'s turn'
     let box = ['', '', '', '', '', '', '', '', '']
+
+    const displayWinner = () => {
+        if (//horizantal
+            ((box[0] == box[1] &&
+                box[1] == box[2] && box[0] == 'X') ||
+                (box[3] == box[4] &&
+                    box[4] == box[5] && box[3] == 'X') ||
+                (box[6] == box[7] &&
+                    box[7] == box[8] && box[6] == 'X')) ||
+            //vetical
+            ((box[0] == box[3] &&
+                box[3] == box[6] && box[0] == 'X') ||
+                (box[1] == box[4] &&
+                    box[4] == box[7] && box[1] == 'X') ||
+                (box[2] == box[5] &&
+                    box[5] == box[8] && box[2] == 'X')) ||
+            //diagnal
+            ((box[0] == box[4] &&
+                box[4] == box[8] && box[0] == 'X') ||
+                (box[2] == box[4] &&
+                    box[4] == box[6] && box[2] == 'X'))) {console.log('x won');winnerHeading.textContent = "X is the winner!";}
+        if (//horizantal
+            ((box[0] == box[1] &&
+                box[1] == box[2] && box[0] == 'O') ||
+                (box[3] == box[4] &&
+                    box[4] == box[5] && box[3] == 'O') ||
+                (box[6] == box[7] &&
+                    box[7] == box[8] && box[6] == 'O')) ||
+            //vetical
+            ((box[0] == box[3] &&
+                box[3] == box[6] && box[0] == 'O') ||
+                (box[1] == box[4] &&
+                    box[4] == box[7] && box[1] == 'O') ||
+                (box[2] == box[5] &&
+                    box[5] == box[8] && box[2] == 'O')) ||
+            //diagnal
+            ((box[0] == box[4] &&
+                box[4] == box[8] && box[0] == 'O') ||
+                (box[2] == box[4] &&
+                    box[4] == box[6] && box[2] == 'O'))) winnerHeading.textContent = "O is the winner!"
+
+        winnerModal.classList.remove('inactive')
+        overlay.classList.remove('inactive')
+    }
+
+    const updatePlayer = () => {
+        if (playerTurn == 'X') {
+            playerTurn = 'O'
+            turnDisplay.textContent = 'O\'s turn'
+        }
+        else {
+            playerTurn = 'X'
+            turnDisplay.textContent = 'X\'s turn'
+        }
+    }
 
     const makeMove = (boxNum) => {
         if (box[boxNum] == '') {
@@ -28,33 +91,47 @@ const board = (() => {
         for (let i = 0; i < 9; i++) {
             box[i] = ''
         }
+        playerTurn = 'O'
         updateBoard()
     }
 
+    const checkOpenBoxes = () => {
+        return !(box.includes(''))
+    }
+
     const checkForWinner = () => {
-        if(//horizantal
-            ((box[0]==box[1] && 
-            box[1]==box[2] && box[0]!='') ||
-            (box[3]==box[4] && 
-                box[4]==box[5] && box[3]!='')||
-                (box[6]==box[7] && 
-                    box[7]==box[8] && box[6]!=''))||
-                    //vetical
-                    ((box[0]==box[3] && 
-                        box[3]==box[6] && box[0]!='') ||
-                        (box[1]==box[4] && 
-                            box[4]==box[7] && box[1]!='')||
-                            (box[2]==box[5] && 
-                                box[5]==box[8] && box[2]!=''))||
-                                //diagnal
-                                ((box[0]==box[4] && 
-                                    box[4]==box[8] && box[0]!='') ||
-                                    (box[2]==box[4] && 
-                                        box[4]==box[6] && box[2]!=''))){
-                                            resetBoard()
-                                            gameSelect.classList.remove('inactive')
-                                            overlay.classList.remove('inactive')
-                                        }
+        if (!box.every(v => v === '')) {
+            if (//horizantal
+                ((box[0] == box[1] &&
+                    box[1] == box[2] && box[0] != '') ||
+                    (box[3] == box[4] &&
+                        box[4] == box[5] && box[3] != '') ||
+                    (box[6] == box[7] &&
+                        box[7] == box[8] && box[6] != '')) ||
+                //vetical
+                ((box[0] == box[3] &&
+                    box[3] == box[6] && box[0] != '') ||
+                    (box[1] == box[4] &&
+                        box[4] == box[7] && box[1] != '') ||
+                    (box[2] == box[5] &&
+                        box[5] == box[8] && box[2] != '')) ||
+                //diagnal
+                ((box[0] == box[4] &&
+                    box[4] == box[8] && box[0] != '') ||
+                    (box[2] == box[4] &&
+                        box[4] == box[6] && box[2] != ''))) {
+                //Winner found
+                displayWinner()
+                resetBoard()
+            }
+            else if (checkOpenBoxes()) {
+                // //game ended in tie
+                console.log('game ended in tie')
+                resetBoard()
+                gameSelect.classList.remove('inactive')
+                overlay.classList.remove('inactive')
+            }
+        }
     }
 
     const updateBoard = () => {
@@ -65,9 +142,7 @@ const board = (() => {
             playerName.textContent = box[i]
             realBoxes[i].appendChild(playerName)
         }
-        if (playerTurn == 'X') playerTurn = 'O'
-        else playerTurn = 'X'
-        
+        updatePlayer()
         checkForWinner()
     }
 
@@ -77,9 +152,33 @@ const board = (() => {
 })();
 
 playBtn.onclick = function () {
-    gameSelect.classList.add('inactive');
-    overlay.classList.add('inactive')
+    if(singlePlayerButton.classList.contains('selected')){
+        gameSelect.classList.add('inactive');
+        overlay.classList.add('inactive')
+        singlePlayerButton.classList.remove('selected')
+    }
+    else if(twoPlayerButton.classList.contains('selected')){
+        gameSelect.classList.add('inactive');
+        overlay.classList.add('inactive')
+        twoPlayerButton.classList.remove('selected')
+    }
 }
+
+continueBtn.onclick = function () {
+    gameSelect.classList.remove('inactive')
+    winnerModal.classList.add('inactive')
+}
+
+singlePlayerButton.onclick = function(){
+    singlePlayerButton.classList.add('selected')
+    twoPlayerButton.classList.remove('selected')
+}
+twoPlayerButton.onclick = function(){
+    twoPlayerButton.classList.add('selected')
+    singlePlayerButton.classList.remove('selected')
+}
+
+
 
 box1.onclick = function () {
     board.makeMove(0)
